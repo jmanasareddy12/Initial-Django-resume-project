f512af02d05bfef35a34d787cd3459aaf96127e0
jmanasareddy <jmanasareddy12@gmail.com>
2026-02-13 16:24:22 +0530

    Initial Django resume project

---
+++ resume/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def projects(request):
+    return render(request,'projects.html')
+
+def skills(request):
+    return render(request,'skills.html')
